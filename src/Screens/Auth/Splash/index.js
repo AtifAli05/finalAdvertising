86090c181e1a933ed019e1c7154f44359c49a292
AtifAli05 <atiali554@gmail.com>
2023-10-24 01:42:43 +0500

    Update index.js

--- src/Screens/Auth/Splash/index.js
+++ src/Screens/Auth/Splash/index.js
@@ -1,52 +1,53 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import AnySvg from '../../../Assets/Images/svg';
-
-import {colors} from '../../../Utils/Constants/Colors';
-import {WP} from '../../../Utils';
+import { colors } from '../../../Utils/Constants/Colors';
 import ButtonPrimary from '../../../Components/Common/ButtonPrimary/ButtonPrimary';
 import SVGComponent from '../../../Assets/Images/svg/Splash-Logo';
-import  SplashImg  from '../../../Assets/Images/svg/SplashImg';
-const Splash = ({navigation}) => {
+import SplashImg from '../../../Assets/Images/svg/SplashImg';
 
+const { width, height } = Dimensions.get('window');
+const logoSize = width * 0.25;
+const marginTopTrack = height * 0.12;
+const marginTopImg = height * 0.05;
+const marginTopButton = height * 0.07;
+const skipFontSize = width * 0.04;
+
+const Splash = ({ navigation }) => {
   return (
     <View style={styles.mainContainer}>
       <AnySvg
-        width={65.28}
-        height={64.79}
+        width={logoSize}
+        height={logoSize}
         name={SVGComponent}
-        style={{marginTop: 165}}
+        style={{ marginTop: marginTopTrack }}
       />
-      <View
-        style={{
-          alignSelf: 'center',
-          marginTop: 67,
-        }}>
+      <View style={{ alignSelf: 'center', marginTop: marginTopTrack }}>
         <Text style={styles.Track}>
           Track and optimize your{' '}
-          <Text style={{color: colors.buttonBackground}}>Advertising </Text>
+          <Text style={{ color: colors.buttonBackground }}>Advertising </Text>
           budget
         </Text>
       </View>
       <View>
         <AnySvg
-          width={9.28}
-          height={1.91}
+          width={width * 0.08}
+          height={height * 0.005}
           name={SplashImg}
-          style={{marginTop: 60, alignSelf: 'center'}}
+          style={{ marginTop: marginTopImg, alignSelf: 'center' }}
         />
       </View>
       <View>
         <ButtonPrimary
           title={'Get Started'}
           style={{
-            marginTop: 50,
+            marginTop: marginTopButton,
             elevation: 6,
             backgroundColor: colors.buttonBackground,
           }}
           buttonPress={() => navigation.navigate('Login')}
         />
-        <Text style={styles.skip}>Skip</Text>
+        <Text style={[styles.skip, { fontSize: skipFontSize }]}>Skip</Text>
       </View>
     </View>
   );
@@ -60,13 +61,12 @@ const styles = StyleSheet.create({
     backgroundColor: colors.backgroundWhite,
   },
   Track: {
-    fontSize: 26,
+    fontSize: width * 0.06,
     fontWeight: '600',
     color: colors.textPrimary,
-    lineHeight: 35,
+    lineHeight: width * 0.085,
   },
   skip: {
-    fontSize: 14,
     fontWeight: '400',
     color: colors.textSecondary,
     textAlign: 'center',
